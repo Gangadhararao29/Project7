8a727137c62efae46359d4f607a844d84196fde1
Gangadhararao29 <gangadhararao29@gmail.com>
2021-04-14 13:12:48 +0530

    login updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,16 +25,19 @@ export class LoginComponent implements OnInit {
   onSubmit(formRef) {
     if (formRef.valid) {
       let loginObj = formRef.value;
-      // console.log(loginObj);
+      console.log(loginObj);
       this.us.loginUser(loginObj).subscribe((res) => {
         if (res['message'] == 'Please enter Username') {
           this.toastr.success('Please enter Username');
         } else if (res['message'] == 'Username not found') {
-          this.toastr.warning('Incorrect Username');
+          this.toastr.warning(
+            'Please check username or sign up to continue',
+            'Username not found'
+          );
           formRef.reset();
         } else if (res['message'] == 'Invalid password') {
           this.toastr.warning('Please enter correct password');
-          formRef.reset();
+          formRef.controls.password.reset();
         } else if (res['message'] == 'login successful') {
           this.toastr.success('Login Successful');
           localStorage.setItem('token', res['token']);
